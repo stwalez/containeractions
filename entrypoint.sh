1dcbf2f895f504bb2d6bdecaadd1d3471b22cf5a
Olawale Abiola <stwalez1@gmail.com>
2022-03-15 15:33:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Hello $INPUT_MYINPUT"
+echo "Hello $INPUT_ANYINPUT"
 memory=$(cat /proc/meminfo)
 echo "::set-output name=memory::$memory" # Enables us to call it in the workflow 
